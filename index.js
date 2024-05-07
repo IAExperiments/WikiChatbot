cf1849ea015158d8780036ad1e0dc01b13295a51
drdonoso <davidrodriguezdonoso95@gmail.com>
2024-05-07 11:36:56 +0200

    Update workflow

--- index.js
+++ index.js
@@ -65,5 +65,5 @@ const chain = RunnableSequence.from([
 var result = await chain.invoke(input);
 
 console.dir(result);
-result = result.replace(/`/g, '``');
+result = result.replace(/`/g, '\\`');
 core.setOutput('answer', result);
